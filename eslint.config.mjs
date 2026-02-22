e8d7753309f92c6a3d9fb7089dc34cfadf384ff0
vakif <88528768+vakifp@users.noreply.github.com>
2026-02-22 14:35:53 +0530

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
-  // Override default ignores of eslint-config-next.
+
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
