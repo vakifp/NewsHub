1cffdce15e6db9e9928a925152ab6b7f5f2ca149
vakif <88528768+vakifp@users.noreply.github.com>
2026-03-08 17:46:15 +0530

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,42 +1,56 @@
-import { db } from "@/lib/firebase";
-import { collection, getDocs } from "firebase/firestore";
-
-export const dynamic = "force-dynamic";
-
-export default async function sitemap() {
-
-  const base = "https://krymoz.com";
-
-  /* POSTS */
-  const postSnap = await getDocs(collection(db,"posts"));
-
-  const posts = postSnap.docs.map(doc => ({
-    url: `${base}/blog/${doc.data().slug || doc.id}`,
-    lastModified: new Date(doc.data().created || Date.now()),
-  }));
-
-
-  /* CATEGORIES */
-  const catSnap = await getDocs(collection(db,"categories"));
-
-  const categories = catSnap.docs
-    .map(d=>d.data())
-    .filter(c=>c.visible)
-    .map(cat => ({
-      url: `${base}/category/${cat.slug}`,
-      lastModified: new Date(),
-    }));
-
-
-  /* STATIC */
-  const staticPages = [
-    { url: base, lastModified: new Date() },
-    { url: `${base}/all-posts`, lastModified: new Date() },
-  ];
-
+export default function sitemap() {
   return [
-    ...staticPages,
-    ...posts,
-    ...categories
+        {
+      url: "https://krymoz.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/latest-tech-updates-you-shouldnt-miss",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/googles-latest-ai-update-what-you-need-to-know",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/how-to-sync-gmail-contacts-to-iphone-easy-safe-steps",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/poco-x2-rear-camera-not-working-fixes-best-gcam-81-setup",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/12-tips-to-improve-your-businesss-financial-management",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/what-is-a-lut-how-to-use-create-lut-filters-for-video-desktop-mobile",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/step-by-step-guide-to-fix-windows-activation-error",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/how-to-fix-the-clash-of-clans-black-screen-quick-safe",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/how-to-fix-windows-11-widgets-not-working-complete-troubleshooting-guide",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/how-to-protect-your-data-on-windows-11",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/install-windows-11-on-any-pc-make-bootable-usb-without-tpm",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://krymoz.com/blog/gta-5-pc-fixes-system-requirements-troubleshooting-guide",
+      lastModified: new Date(),
+    },
   ];
 }
